0a13dd690dbbb4bcd7eaf0dca538921cfbaed110
Federico Paolinelli <fedepaol@gmail.com>
2019-02-10 16:40:41 +0100

    Changed comment as per lint

--- story/routing.go
+++ story/routing.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-// Handler returns a handler func that handles the story
+// NewHandler returns a handler func that handles the story
 func NewHandler(s Story, start Node, baseURL string, rend Renderer) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path[1:]
